9e575cd79531444894a0479b9a937566ab94ffcc
Mateusz Stankiewicz <mateusz.stankiewicz26@gmail.com>
2024-11-11 22:23:10 +0100

    Wm_paint

--- main.cpp
+++ main.cpp
@@ -10,6 +10,17 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     case WM_DESTROY:
         PostQuitMessage(0);
         return 0;
+
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+
+        // All painting occurs here, between BeginPaint and EndPaint.
+        FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
+        EndPaint(hwnd, &ps);
+    }
+        return 0;
     }
 
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
@@ -18,7 +29,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 int main()
 {
     // Register the window class.
-    const char* class_name = "chuj";
+    const char* class_name = "Test";
     const wchar_t CLASS_NAME[]  = L"Sample Window Class";
 
     WNDCLASS wc = { };
