f327f8c5a949e5da16610cb56b2f407d88dadd6d
Olek <uchihaolek@gmail.com>
2024-11-14 10:41:09 -0800

    window.cpp window.h

---
+++ window.cpp
@@ -0,0 +1,40 @@
+#include "window.h"
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+
+    case WM_CLOSE:
+    {
+    	cout << "WM_CLOSE" << endl;
+    	DestroyWindow(hwnd);
+    	return 0;
+    }
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+
+        // All painting occurs here, between BeginPaint and EndPaint.
+        for (int y = 0; y < 200; y++){
+            for (int x=100; x < 400; x++)
+            {
+                SetPixel(hdc, x, y, RGB(255, 0, 0));
+            }
+        }
+
+
+        FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
+        EndPaint(hwnd, &ps);
+        return 0;
+    }
+        
+    }
+
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
---
+++ window.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
