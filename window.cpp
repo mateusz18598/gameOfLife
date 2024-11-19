19f17b4f0fe1ee63d65c54733dd420b90f800005
Mateusz Stankiewicz <mateusz.stankiewicz26@gmail.com>
2024-11-19 22:44:31 +0100

    working window?

--- window.cpp
+++ window.cpp
@@ -95,6 +95,8 @@ static LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPar
             cout << "nie ma frame.pixels" << endl;
         }
 
+        FillRect(hdc, &rect, (HBRUSH)(COLOR_WINDOW + 1));  
+
         EndPaint(hwnd, &ps);
 
         return 0;
@@ -106,13 +108,13 @@ static LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPar
 
         bmpInfo.bmiHeader.biWidth = frame.x;
         bmpInfo.bmiHeader.biHeight = frame.y;
-
-        if (frame.pixels) delete (frame.pixels);
-        frame.pixels = new uint8_t(frame.x * frame.y * 4);
+         
+        if (frame.pixels) delete(frame.pixels);
+        frame.pixels = new uint8_t[frame.x * frame.y * 4];
 
         return 0;
     }
-
+    default:
         return DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
 }
